dcdb9e74c2c24ef1075dc070225c233b55fec704
iansaggior <110851341+iansaggior@users.noreply.github.com>
2023-02-27 00:14:17 -0300

    Update Ado1.java

--- AdoPoo1/Ado1.java
+++ AdoPoo1/Ado1.java
@@ -10,72 +10,106 @@ import java.util.*;
 public class Ado1 {
     public static void main(String[] args) {
         String linha = null;
-        String arquivoEntrada = "C:../AdoPoo1/pib.txt";
-
-        /* ------------------------------------- */
-        /* Abertura de arquivo e loop de leitura */
-        /* ------------------------------------- */
-
-        ArrayList<Informacao> estados = new ArrayList<Informacao>();
+        ArrayList <Informacao> estados = new ArrayList <Informacao> ();
         float soma = 0;
+        String arquivoEntradaPib = "C:../AdoPoo1/pib.txt";
+        String arquivoEntradaRegioes = "C:../AdoPoo1/regioes.txt";
+        String linha1 = null;
+        ArrayList <Informacao> regioesBrasil = new ArrayList <Informacao> ();
+        String arquivoSaida = "C:../AdoPoo1/saida.txt";
+        String states = estados.toString();
+        
+/*      --------------------------------------------    */
+/*      Abertura do arquivo pib.txt e loop de leitura   */
+/*      --------------------------------------------    */
+
+
         try {
-            FileReader leitorDeArquivo = new FileReader(arquivoEntrada);
+            FileReader leitorDeArquivo = new FileReader(arquivoEntradaPib);
             BufferedReader bufferDeArquivo = new BufferedReader(leitorDeArquivo);
-
-            while ((linha = bufferDeArquivo.readLine()) != null) {
-                String[] dados = linha.split(";");
-
+            
+            while((linha = bufferDeArquivo.readLine()) != null) {
+                String [] dados = linha.split(";");
                 Informacao informacao = new Informacao();
+
+                //Extrair os nomes dos estados
                 informacao.setNomeEstado(dados[0]);
+                //Extrair os PIBs dos estados
                 informacao.setPib(Float.parseFloat(dados[1]));
+                //Fazer a soma de todos os PIBs
                 informacao.setSoma(soma += informacao.getPib());                    
-
+                
                 estados.add(informacao);
-
-                /*
-                 * usar os dados ja separados no split, pegando o pib d cada estado e dividir
-                 * com a variavel soma e multiplicar por 100
-                 */
             }
-            System.out.println(estados);
-            System.out.println("Total: " + soma);
+            
+            System.out.println("PIB do Brasil: " + soma + " Trilhões\n");                
             bufferDeArquivo.close();
 
-        } catch (FileNotFoundException ex) {
-            System.out.println("Arquivo inexistente: '" + arquivoEntrada + "'");
-        } catch (IOException ex) {
-            System.out.println("Erro lendo o arquivo '" + arquivoEntrada + "'");
         }
-        System.out.println();
-       
-        for (Informacao i: (estados)) {
-            float porcentagem = (i.getPib() / soma) * 100;
-            System.out.println(porcentagem);
+        catch(FileNotFoundException ex) {
+            System.out.println("Arquivo inexistente: '" + arquivoEntradaPib + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println("Erro lendo o arquivo '" + arquivoEntradaPib + "'");                  
         }
 
+        //Realizar a porcentagem do PIB de cada estado do Brasil
+        for (Informacao i: estados) {
+            i.setPorcentagem((i.getPib() / soma) * 100);
+            System.out.println(i);
+        }
 
-        /* ------------------------------------- */
-        /* Exemplo de escrita em arquivo */
-        /* ------------------------------------- */
+/*      ------------------------------------------------- */
+/*      Abertura do arquivo regioes.txt e loop de leitura */
+/*      ------------------------------------------------- */
 
-        String arquivoSaida = "C:../AdoPoo1/saida.txt";
+
         try {
+            FileReader flrd = new FileReader(arquivoEntradaRegioes);
+            BufferedReader bfrd = new BufferedReader(flrd);
+            
+            while((linha1 = bfrd.readLine()) != null) {
+                String[] regiao = linha1.split(System.lineSeparator());
+                Informacao informacao = new Informacao();
+                //informacao.setRegiao(regiao);
+
+                System.out.print(Arrays.toString(regiao).replace("[]", "\n"));
+                regioesBrasil.add(informacao);
+            }
+            
+            bfrd.close();
+
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Arquivo inexistente: '" + arquivoEntradaRegioes + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println("Erro lendo o arquivo '" + arquivoEntradaRegioes + "'");                  
+        }
+
+
+        
+/*      -------------------------------------   */
+/*      Escrita no arquivo .txt                 */
+/*      -------------------------------------   */
+
+
+        states = states.replace("[", "");
+        states = states.replace("]", "");
+        states = states.replace(", ", "\n");
+        try {
+            
             FileWriter fileWriter = new FileWriter(arquivoSaida);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            PrintWriter imprimirArquivo = new PrintWriter(bufferedWriter);
-
-            bufferedWriter.write("pib da regiao X = $$$$");
-            bufferedWriter.newLine();
-            bufferedWriter.write("pib da regiao Y = $$$$");
-            bufferedWriter.newLine();
-            // bufferedWriter.write("Estados: " + estados);
-            imprimirArquivo.append("\nEstados: \n" + estados);
-
+            PrintWriter escritorDeArquivo = new PrintWriter(bufferedWriter);
+            
+                escritorDeArquivo.append("PIB do Brasil: " + soma + " Trilhoes\n");
+                escritorDeArquivo.append("Estados \t - \t PIB \t - \tPorcentagem \n\n" + states);
+                
             bufferedWriter.close();
-
-        } catch (IOException ex) {
+        }
+        catch(IOException ex) {
             System.out.println("Erro de escrita em '" + arquivoSaida + "'");
         }
-
     }
 }
